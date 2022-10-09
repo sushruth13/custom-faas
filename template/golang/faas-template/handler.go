faf6bab176c424f169aa34127f3c20f85d1e2990
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2022-10-09 09:28:21 -0400

    Add faas-template

---
+++ template/golang/faas-template/handler.go
@@ -0,0 +1,65 @@
+package function
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	handler "github.com/openfaas/templates-sdk/go-http"
+)
+
+var (
+	subject        = "nats-test"
+	defaultMessage = "Relay test"
+)
+
+// Handle a serverless request
+func Handle(req handler.Request) (handler.Response, error) {
+	log.Printf("Received: %q", string(req.Body))
+
+	// if val, ok := os.LookupEnv("wait"); ok && len(val) > 0 {
+	// 	parsedVal, _ := time.ParseDuration(val)
+	// 	log.Printf("Waiting for %s before returning", parsedVal.String())
+	// 	time.Sleep(parsedVal)
+	// }
+
+	msg := defaultMessage
+
+	natsURL := nats.DefaultURL
+	val, ok := os.LookupEnv("nats_url")
+	if ok {
+		natsURL = val
+	}
+
+	nc, err := nats.Connect(natsURL)
+	if err != nil {
+		errMsg := fmt.Sprintf("can not connect to nats: %s", err)
+		log.Printf(errMsg)
+		r := handler.Response{
+			Body:       []byte(errMsg),
+			StatusCode: http.StatusInternalServerError,
+		}
+		return r, err
+	}
+	defer nc.Close()
+
+	log.Printf("Publishing %d bytes to: %q\n", len(msg), subject)
+
+	err = nc.Publish(subject, []byte(msg))
+	if err != nil {
+		log.Println(err)
+
+		r := handler.Response{
+			Body:       []byte(fmt.Sprintf("can not publish to nats: %s", err)),
+			StatusCode: http.StatusInternalServerError,
+		}
+		return r, err
+	}
+
+	return handler.Response{
+		Body:       []byte(fmt.Sprintf("Received: %q", string(req.Body))),
+		StatusCode: http.StatusOK,
+	}, nil
+}
