20a0ea6e37473abb3b692b4e4f7846f98972a085
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2022-11-03 13:56:12 -0400

    Update golang to reciever

--- template/golang/function/handler.go
+++ template/golang/function/handler.go
@@ -1,73 +1,23 @@
 package function
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	nats "github.com/nats-io/nats.go"
 	handler "github.com/openfaas/templates-sdk/go-http"
 )
 
-var (
-	subject        = "nats-test"
-	defaultMessage = "Relay test"
-)
-
-type request struct {
-	Body string
-}
-
 // Handle a serverless request
-func Handle(bytereq []byte) (handler.Response, error) {
-	var req request
-
-	err := json.Unmarshal(bytereq, &req)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
+func Handle(req handler.Request) (handler.Response, error) {
 	log.Printf("Received: %q", string(req.Body))
 
-	// if val, ok := os.LookupEnv("wait"); ok && len(val) > 0 {
-	// 	parsedVal, _ := time.ParseDuration(val)
-	// 	log.Printf("Waiting for %s before returning", parsedVal.String())
-	// 	time.Sleep(parsedVal)
-	// }
-
-	msg := defaultMessage
-
-	natsURL := nats.DefaultURL
-	val, ok := os.LookupEnv("nats_url")
-	if ok {
-		natsURL = val
-	}
-
-	nc, err := nats.Connect(natsURL)
-	if err != nil {
-		errMsg := fmt.Sprintf("can not connect to nats: %s", err)
-		log.Printf(errMsg)
-		r := handler.Response{
-			Body:       []byte(errMsg),
-			StatusCode: http.StatusInternalServerError,
-		}
-		return r, err
-	}
-	defer nc.Close()
-
-	log.Printf("Publishing %d bytes to: %q\n", len(msg), subject)
-
-	err = nc.Publish(subject, []byte(msg))
-	if err != nil {
-		log.Println(err)
-
-		r := handler.Response{
-			Body:       []byte(fmt.Sprintf("can not publish to nats: %s", err)),
-			StatusCode: http.StatusInternalServerError,
-		}
-		return r, err
+	if val, ok := os.LookupEnv("wait"); ok && len(val) > 0 {
+		parsedVal, _ := time.ParseDuration(val)
+		log.Printf("Waiting for %s before returning", parsedVal.String())
+		time.Sleep(parsedVal)
 	}
 
 	return handler.Response{
