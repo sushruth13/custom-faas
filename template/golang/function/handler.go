1ccfe48b608be95ed30c2c9b7cdfa3bc5289ad1f
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2022-11-03 14:27:47 -0400

    :)

--- template/golang/function/handler.go
+++ template/golang/function/handler.go
@@ -1,20 +1,77 @@
 package function
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	nats "github.com/nats-io/nats.go"
 	handler "github.com/openfaas/templates-sdk/go-http"
 )
 
-// Handle a function invocation
-func Handle(req handler.Request) (handler.Response, error) {
-	var err error
+var (
+	subject        = "nats-test"
+	defaultMessage = "Hello World"
+)
 
-	message := fmt.Sprintf("Body: %s", string(req.Body))
+// Handle a serverless request
+func Handle(req handler.Request) (handler.Response, error) {
+	msg := defaultMessage
+	if len(req.Body) > 0 {
+		msg = string(bytes.TrimSpace(req.Body))
+	}
+
+	natsURL := nats.DefaultURL
+	val, ok := os.LookupEnv("nats_url")
+	if ok {
+		natsURL = val
+	}
+
+	nc, err := nats.Connect(natsURL)
+	if err != nil {
+		errMsg := fmt.Sprintf("can not connect to nats: %s", err)
+		log.Printf(errMsg)
+		r := handler.Response{
+			Body:       []byte(errMsg),
+			StatusCode: http.StatusInternalServerError,
+		}
+		return r, err
+	}
+	defer nc.Close()
+
+	log.Printf("Publishing %d bytes to: %q\n", len(msg), subject)
+
+	err = nc.Publish(subject, []byte(msg))
+	if err != nil {
+		log.Println(err)
+
+		r := handler.Response{
+			Body:       []byte(fmt.Sprintf("can not publish to nats: %s", err)),
+			StatusCode: http.StatusInternalServerError,
+		}
+		return r, err
+	}
 
 	return handler.Response{
-		Body:       []byte(message),
+		Body:       []byte(fmt.Sprintf("Published %d bytes to: %q", len(msg), subject)),
 		StatusCode: http.StatusOK,
-	}, err
+	}, nil
 }
+
+//Recieve
+// func Handle(req handler.Request) (handler.Response, error) {
+// 	log.Printf("Received: %q", string(req.Body))
+
+// 	if val, ok := os.LookupEnv("wait"); ok && len(val) > 0 {
+// 		parsedVal, _ := time.ParseDuration(val)
+// 		log.Printf("Waiting for %s before returning", parsedVal.String())
+// 		time.Sleep(parsedVal)
+// 	}
+
+// 	return handler.Response{
+// 		Body:       []byte(fmt.Sprintf("Received: %q", string(req.Body))),
+// 		StatusCode: http.StatusOK,
+// 	}, nil
+// }
