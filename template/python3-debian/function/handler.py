3482fc182e0616bd18e2ef98281da8d7fbe06a83
sushruth13 <hsushruth@gmail.com>
2023-01-24 20:02:39 -0500

    changed template for python3

--- template/python3-debian/function/handler.py
+++ template/python3-debian/function/handler.py
@@ -1,10 +1,15 @@
 import requests
+from time import sleep
+import random
 
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    outputStr="f1,"+"Start"
+    sleep(random.randint(1,10))
+    
+    outputStr="wf1-f1,"+"Start"
     requests.post("http://172.22.85.31:30036",outputStr)
-    return "Sent request to the coordinator from f1"
+    #data=requests.post("http://172.22.85.31:31112/function/f2","1")
+    return "Sent the request to coordinator from wf1-f1"
