fd3721e674d7106eb1394d4e2533ccb7d01a5216
sushruth13 <37882612+sushruth13@users.noreply.github.com>
2024-01-05 22:46:36 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,21 @@
 import requests
 import uuid
-
+from time import sleep
+import random
 
 
 def main():
-    return "Hello World"
+    sleep_duration = random.uniform(5, 10)
+    sleep(sleep_duration)
+    #sending postrequest to recorder
+    recorder_url="http://172.22.85.201:31082/record"
+
+    #sends data to the recorder
+    data=requests.post(recorder_url,combined_data)
+
+    #sending data to Orachestrator
+
+
+    orachestrator_url="http://172.22.85.201:30578/orachestra"
+    data=requests.post(orachestrator_url,combined_data)
 
